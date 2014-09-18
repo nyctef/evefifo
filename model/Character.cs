995ac5cd9c6f6221f077bd0ad6ce0ee79e8e8505
Nyctef <nyctef@nyctef.com>
2014-09-18 02:14:17 +0100

    seems sqlite wants these to be longs

--- model/Character.cs
+++ model/Character.cs
@@ -9,11 +9,11 @@ namespace evefifo.model
     public class Character
     {
         public string CloneName { get; set; }
-        public int CloneSP { get; set; }
+        public long CloneSP { get; set; }
         public string CorpName { get; set; }
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public double SecStatus { get; set; }
-        public int SP { get; set; }
+        public long SP { get; set; }
     }
 }
