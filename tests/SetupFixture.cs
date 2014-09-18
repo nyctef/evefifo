d59bfd396fbc536ad2456a2b89e449d33e55f061
Nyctef <nyctef@nyctef.com>
2014-09-18 20:38:10 +0100

    add a setup fixture which resets the database

---
+++ tests/SetupFixture.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests
+{
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Recreate an empty sqlite database for the tests to run against
+
+            AppDomain.CurrentDomain.SetData("DataDirectory", @".\");
+
+            const string db = "evefifo.sqlite";
+            if (File.Exists(db))
+            {
+                File.Delete(db);
+            }
+            SQLiteConnection.CreateFile(db);
+            using (var conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["EvefifoContext"].ConnectionString))
+            using (var command = conn.CreateCommand())
+            {
+                conn.Open();
+                command.CommandText = File.ReadAllText(@"..\..\..\model\scripts\create-sqlite-db.sql");
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
