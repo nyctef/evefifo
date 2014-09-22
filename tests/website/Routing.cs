7fd564e62750837c45964d1c782297dd4222d7e0
Nyctef <nyctef@nyctef.com>
2014-09-23 00:24:48 +0100

    we can test the website!
    this might be the coolest thing about OWIN

---
+++ tests/website/Routing.cs
@@ -0,0 +1,25 @@
+﻿using evefifo.website;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using System.Net;
+
+namespace evefifo.tests.website
+{
+    public class Routing
+    {
+        [Test]
+        public async void RootPathReturns200()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+    }
+}
