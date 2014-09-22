a0d41710f702df55421108ab3e34e051a036571d
Nyctef <nyctef@nyctef.com>
2014-09-23 00:18:19 +0100

    Add a minimal OWIN website project

---
+++ website/Startup.cs
@@ -0,0 +1,12 @@
+using Owin;
+
+namespace evefifo.website
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
